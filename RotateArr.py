97ce1cd50c7e81663c454849830152ef1a4cc91e
Munir Mohammed <munirmohammed038@gmail.com>
2022-03-28 20:16:21 +0300

    Create RotateArr.py

---
+++ RotateArr.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+       
+        k = k % len(nums)
+        nums[:] = self.reverse(nums)
+        nums[:k] = self.reverse(nums[:k])
+        nums[k:] = self.reverse(nums[k:])
+        
+    def reverse(self,arr):
+        l = 0
+        r = len(arr) - 1
+        while l < r:
+            arr[l],arr[r] = arr[r],arr[l]
+            l += 1
+            r -= 1
+        return arr
