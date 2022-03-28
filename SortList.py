1d5412a5a009fa7aed18ba483ff30fc01b8a8083
Munir Mohammed <munirmohammed038@gmail.com>
2022-03-28 20:06:17 +0300

    Create SortList.py

---
+++ SortList.py
@@ -0,0 +1,22 @@
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head: return head
+        
+        #convert linked list to array
+        arr = []
+        curr = head
+        while curr:
+            arr.append(curr.val)
+            curr = curr.next
+        
+        #sort the array
+        arr.sort()
+        
+        #convert array back to linked list
+        head = ListNode(arr[0])
+        curr = head
+        for i in range(1,len(arr)):
+            newNode = ListNode(arr[i])
+            curr.next = newNode
+            curr = newNode
+        return head
