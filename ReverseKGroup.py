bf730916b98d768d4316ce734a2392cba4683ae4
Munir Mohammed <munirmohammed038@gmail.com>
2022-03-28 19:50:54 +0300

    Create ReverseKGroup.py

---
+++ ReverseKGroup.py
@@ -0,0 +1,28 @@
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        num = 0
+        curr = head
+        while curr:
+            curr = curr.next
+            num += 1
+        def reverse(head,count,num):
+            if not head: return head
+            if num - count < k: return head
+            m = 0
+            node = head
+            while m < k:
+                node = node.next
+                m += 1
+            temp = head.next
+            head.next = reverse(node,(count+k),num)
+            prev = head
+            curr = temp
+            n = 2
+            while curr and n <= k:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+                n += 1
+            return prev
+        return reverse(head,0,num)
