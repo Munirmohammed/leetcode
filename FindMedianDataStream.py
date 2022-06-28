0383bda3ad34fb182f36fef1d5d29867ce65c191
Munir Mohammed <munirmohammed038@gmail.com>
2022-06-28 15:03:46 +0300

    Create FindMedianDataStream.py

---
+++ FindMedianDataStream.py
@@ -0,0 +1,26 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.small = []  
+        self.large = []
+        
+
+    def addNum(self, num: int) -> None:
+        if len(self.small) == len(self.large):
+            heappush(self.large, -heappushpop(self.small, -num))
+        else:
+            heappush(self.small, -heappushpop(self.large, num))
+        
+
+    def findMedian(self) -> float:
+        if len(self.small) == len(self.large):
+            return float(self.large[0] - self.small[0]) / 2.0
+        else:
+            return float(self.large[0])
+        
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
