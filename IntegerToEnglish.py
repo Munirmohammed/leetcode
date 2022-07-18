bdf7d85541ed735a4dc6b3b73112a4f72281dc79
Munir Mohammed <munirmohammed038@gmail.com>
2022-07-18 12:45:04 +0300

    Update IntegerToEnglish.py

--- IntegerToEnglish.py
+++ IntegerToEnglish.py
@@ -1,15 +1,39 @@
 class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        if n == 0:
-            return 1
+    def numberToWords(self, num: int) -> str:
+        powers=['Thousand','Million','Billion']
+        digitword=[]
+        for i in range(3,0,-1):
+            sendee=num// 10**(3*i)
+            digit=self.threedigit(sendee)
+            if digit!="Zero":
+                digitword.append(digit)
+                digitword.append(powers[i-1])
+            num=num% 10**(3*i)
+        digit=self.threedigit(num)
+        if digit=="Zero":
+            if len(digitword)==0:
+                digitword.append(digit)
         else:
-            if n < 0:
-                if n%2 == 0:
-                    return self.myPow(x, n / 2) ** 2
-                else:
-                    return (1/x) * self.myPow(x, (n+1) / 2) ** 2
-            else:
-                if n%2 == 0:
-                    return self.myPow(x, n / 2) ** 2
-                else:
-                    return x * self.myPow(x, (n-1) / 2) ** 2
+            digitword.append(digit)
+            
+        print(digitword)
+        return " ".join(digitword)
+    
+    def threedigit(self,num):
+        tens=['Twenty','Thirty','Forty','Fifty','Sixty','Seventy','Eighty','Ninety']
+        ones=['One','Two','Three','Four','Five','Six','Seven','Eight','Nine']
+        teens=['Ten','Eleven','Twelve','Thirteen','Fourteen','Fifteen','Sixteen','Seventeen','Eighteen','Nineteen']
+        digitword=[]
+        if num==0:
+            return "Zero"
+        if num//100!=0:
+            digitword.append(ones[num//100-1])
+            digitword.append("Hundred")
+        if num%100<20 and num%100>=10:
+            digitword.append(teens[num%10])
+            return " ".join(digitword)
+        if (num%100)//10!=0:
+            digitword.append(tens[(num%100)//10-2])
+        if num%10!=0 :
+             digitword.append(ones[num%10-1])
+        return " ".join(digitword)
